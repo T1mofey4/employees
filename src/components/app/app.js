6465ce8cc96c36201423d538f636c01facf87e1d
Timofey <timofeyvalerievich@gmail.com>
2022-05-19 22:48:49 +0300

    added cheking for empty stringsin forms

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -14,9 +14,9 @@ class App extends Component {
         super(props);
         this.state = {
             data: [
-                {name: "John C.", salary: 800, increase: false, id: 1},
-                {name: "Alex M.", salary: 3000, increase: true, id: 2},
-                {name: "Carl W.", salary: 5000, increase: false, id: 3}
+                {name: "John C.", salary: 800, increase: false, rise: true, id: 1},
+                {name: "Alex M.", salary: 3000, increase: true, rise: false, id: 2},
+                {name: "Carl W.", salary: 5000, increase: false, rise: false, id: 3}
             ]
         }
         this.maxId = 4;
@@ -36,21 +36,69 @@ class App extends Component {
             name,
             salary,
             increase: false,
+            rise: false,
             id: this.maxId++
         }
 
-        this.setState(({data}) => {
-            const newArr = [...data, newItem];
-            return {
-                data: newArr
-            }
-        });
+        const regEx = /^[а-яА-ЯёЁa-zA-Z. ]+$/;
+    
+        if (newItem.name.length === 0) {
+            return alert('Введите имя сотрудника!')
+        } else if (newItem.salary.length === 0) {
+            return alert('Укажите размер зарплаты сотрудника!')
+        } else if (regEx.test(newItem.name) === false) {
+            return alert('Недопустимое значение имени сотрудника!') 
+        } else {
+            this.setState(({data}) => {
+                const newArr = [...data, newItem];
+                return {
+                    data: newArr
+                }
+            });
+        }
+    }
+
+    onToggleIncrease = (id) => {
+        // this.setState(({data}) => {
+        //     const index = data.findIndex(elem => elem.id === id);
+
+        //     const old = data[index];
+        //     const newItem = {...old, increase: !old.increase};
+        //     const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+
+        //     return {
+        //         data: newArr
+        //     }
+        // })
+
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, increase: !item.increase}
+                }
+                return item;
+            })
+        }))
+    }
+
+    onToggleRise = (id) => {
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, rise: !item.rise}
+                }
+                return item;
+            })
+        }))
     }
 
     render() {
+        const employeeCount = this.state.data.length;
+        const increased = this.state.data.filter(item => item.increase).length;
+
         return (
             <div className = "app">
-                <AppInfo/>
+                <AppInfo employeeCount={employeeCount} increased={increased}/>
     
                 <div className="search-panel">
                     <SearchPanel/>
@@ -59,7 +107,9 @@ class App extends Component {
     
                 <EmployeesList 
                     data={this.state.data}
-                    onDelete={this.deleteItem}/>
+                    onDelete={this.deleteItem}
+                    onToggleIncrease={this.onToggleIncrease}
+                    onToggleRise={this.onToggleRise}/>
                 <EmployeesAddForm onAdd={this.addItem}/>
             </div>
         );
@@ -67,3 +117,18 @@ class App extends Component {
 }
 
 export default App;
+
+// const data = [
+//         {name: "John C.", salary: 800, increase: false, rise: true, id: 1},
+//         {name: "Alex M.", salary: 3000, increase: true, rise: false, id: 2},
+//         {name: "Carl W.", salary: 5000, increase: false, rise: false, id: 3}
+//     ]
+
+// const newName = 'John C.'    
+// const checkCo = data.filter(item => item.name == newName) 
+
+// console.log(checkCo.length)
+
+// if (checkCo.length > 0) {
+//     console.log('уже есть')
+// }    
--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,52 +1,27 @@
-import { Component } from 'react';
-
 import './employees-list-item.css'
 
-class EmployeesListItem extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            increase: false,
-            like: false
-        }
-    }
+const EmployeesListItem = (props) => {
 
-    
-    onIncrease = () => {
-        this.setState(({increase}) => ({
-            increase: !increase
-        }))
-    }
-
-    onLike = () => {
-        this.setState(({like}) => ({
-            like: !like
-        }))
-    }
-    
-    render() {
-        const {name, salary, onDelete} = this.props;
-        const {increase} = this.state;
-        const {like} = this.state;
+    const {name, salary, onDelete, onToggleIncrease, onToggleRise, increase, rise} = props;
 
         let classNames = "list-group-item d-flex justify-content-between";
         if (increase) {
             classNames += ' increase';
         }
-        if (like) {
+        if (rise) {
             classNames += ' like';
         }
     
         return (
             <li className={classNames}>
                 <span className="list-group-item-label" 
-                onClick={this.onLike}>{name}
+                onClick={onToggleRise}>{name}
                 </span>
                 <input type="text" className="list-group-item-input" defaultValue={salary + " $"}/>
                 <div className='d-flex justify-content-center align-items-center'>
                     <button type="button"
                         className="btn-cookie btn-sm "
-                        onClick={this.onIncrease}>
+                        onClick={onToggleIncrease}>
                         <i className="fas fa-cookie"></i>
                     </button>
     
@@ -60,7 +35,7 @@ class EmployeesListItem extends Component {
             </li>
     
         );
-    }
+
 }
 
 export default EmployeesListItem;
